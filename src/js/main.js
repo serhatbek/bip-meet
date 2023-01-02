278a2963729e2cf6da112a202eb67f52130c3f85
serhatbek <serhat.bekk@gmail.com>
2023-01-02 10:14:12 +0300

    responsive header & dropdown

---
+++ src/js/main.js
@@ -0,0 +1,68 @@
+const dropdownBtns = document.querySelectorAll('.js-dropdown-btn');
+const dropdownItems = document.querySelectorAll('.dropdown');
+const menuToggleBtn = document.querySelector('.header__menu-toggle');
+const menuToggleIcon = document.querySelector('.header__menu-toggle img');
+const header = document.querySelector('.header');
+const headerNav = document.querySelector('nav');
+const headerBtns = document.querySelectorAll('.button--small');
+
+if (dropdownItems) {
+  window.addEventListener('click', (e) => {
+    let target = e.target.closest('.js-dropdown-btn');
+    if (!target) {
+      dropdownBtns.forEach((btn) => btn.classList.remove('active'));
+    }
+  });
+}
+
+const toggleMenu = () => {
+  if (headerNav.classList.contains('active')) {
+    headerNav.classList.remove('active');
+    header.classList.remove('active');
+    menuToggleIcon.src = '../assets/images/icons-menu-open.png';
+    headerBtns.forEach((btn) => btn.classList.remove('button--large'));
+  } else {
+    headerNav.classList.add('active');
+    header.classList.add('active');
+    menuToggleIcon.src = '../assets/images/icons-menu-close.png';
+    headerBtns.forEach((btn) => btn.classList.add('button--large'));
+  }
+};
+
+if (menuToggleBtn) {
+  menuToggleBtn.addEventListener('click', toggleMenu);
+}
+
+if (dropdownBtns) {
+  dropdownBtns.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+      let target = e.currentTarget;
+      if (!target.classList.contains('active')) {
+        dropdownBtns.forEach((item) => {
+          item.classList.remove('active');
+        });
+        target.classList.add('active');
+      } else {
+        target.classList.remove('active');
+      }
+    });
+  });
+}
+
+const mobileMediaQuery = window.matchMedia('(min-width: 1110px)');
+const checkMobileQuery = (e) => {
+  if (e.matches && headerNav.classList.contains('active')) {
+    toggleMenu();
+  }
+};
+
+mobileMediaQuery.addEventListener('change', checkMobileQuery);
+checkMobileQuery(mobileMediaQuery);
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 0) {
+    header.classList.add('header--sticky');
+  } else {
+    header.classList.remove('header--sticky');
+  }
+});
